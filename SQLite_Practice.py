887b3f106c032337c70f8e37c2ffdef7156001c0
veepham <111474183+veepham@users.noreply.github.com>
2022-12-06 11:13:13 -0800

    SQLite Practice

---
+++ SQLite_Practice.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+peopleValues = (('Ron','Obvious',42), ('Luigi','Vercotti',43), ('Arthur','Belling',28))
+
+with sqlite3.connect('test_database.db') as connection:
+    c = connection.cursor()
+    c.execute("DROP TABLE IF EXISTS People")
+    c.execute("CREATE TABLE People(FirstName TEXT, LastName TEXT, Age INT)")
+    c.executemany("INSERT INTO People VALUES (?,?,?)",
+                  peopleValues)
+
+    c.execute("SELECT FirstName,LastName FROM People WHERE Age > 30")
+    for row in c.fetchall():
+        print(row)
+    c.execute("SELECT FirstName,LastName FROM People WHERE Age > 30")
+    while True:
+        row = c.fetchone()
+        if row is None:
+            break
+        print(row)
